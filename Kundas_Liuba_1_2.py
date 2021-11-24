5461e8bcf0b8584649ae59bc3704f955ffaf91d5
Liuba-K <67833767+Liuba-K@users.noreply.github.com>
2021-11-24 14:02:29 +0300

    Update Kundas_Liuba_1_2.py
    modified task 2

--- Kundas_Liuba_1_2.py
+++ Kundas_Liuba_1_2.py
@@ -5,34 +5,23 @@ while idx <= 1000: # счетчик
         cub_odd.append(idx**3)
     idx += 1
 
-seven_num = 0
-for num in cub_odd:
+for num, item in enumerate(cub_odd):
     num_total = 0
-    num_copia = num
-    while num // 10 > 10:
-        remainder = num % 10
-        whole = num // 10
-        num_total += remainder
-        num = whole
-    remainder = num % 10
-    whole = num // 10
-    num_total = num_total + remainder + whole
+    while item > 0:
+        num_total += item % 10
+        item //= 10
     if num_total % 7 == 0:
-        seven_num += num_copia
-print(seven_num)
+        seven_num += cub_odd[num]
+
 seven_num2 = 0
-for num, item in enumerate (cub_odd):
+for num, item in enumerate(cub_odd):
     item += 17
     num_total = 0
     num_copia = item
-    while item // 10 > 10:
-        remainder = item % 10
-        whole = item // 10
-        num_total += remainder
-        item = whole
-    remainder = item % 10
-    whole = item // 10
-    num_total = num_total + remainder + whole
+    while item > 0:
+        num_total += item % 10
+        item //= 10
     if num_total % 7 == 0:
-        seven_num2 += num_copia
-print(seven_num2)
+        seven_num2 += cub_odd[num]
+
+print(seven_num, seven_num2)
