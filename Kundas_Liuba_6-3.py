a7ac62f3c141220134000c504089809ceffb3902
Liuba Kundas <kundasl1@gmail.com>
2021-12-13 23:51:43 +0300

    modified task 6-3

--- Kundas_Liuba_6-3.py
+++ Kundas_Liuba_6-3.py
@@ -1,15 +1,17 @@
+from itertools import zip_longest
 
 with open("users.csv", "r", encoding="utf-8") as user:
     user_lines = []
     for line in user:
         user_lines.append(line.replace(",", " ").replace("\n", ""))
-print(user_lines)
+    with open("hobby.csv", "r", encoding="utf-8") as hobby:
+        hobby_lines = []
+        for line in hobby:
+            hobby_lines.append(line.replace("\n", ""))
 
-with open("hobby.csv", "r", encoding="utf-8") as hobby:
-    hobby_lines = []
-    for line in hobby:
-        hobby_lines.append(line.replace("\n", ""))
-for k, g in zip(user_lines, hobby_lines):
-    print(f"{k}, {g}")
+result = (line for line in zip_longest(user_lines, hobby_lines) if len(user_lines) > len(hobby_lines))
+for line in result:
+    print(line)
+
 
 
