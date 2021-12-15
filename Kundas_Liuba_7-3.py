c6bd4cd6f6a4b08a12c1fad654aac730b7a26aae
Liuba Kundas <kundasl1@gmail.com>
2021-12-15 12:18:33 +0300

    modified task 7-3

--- Kundas_Liuba_7-3.py
+++ Kundas_Liuba_7-3.py
@@ -2,11 +2,11 @@ import os
 import shutil
 
 try:
-    for root, dirs, files in os.walk("my_project_1"):
+    for root, dirs, files in os.walk("my_project"):
         if "templates" in dirs:
             for i in os.listdir(os.path.join(root, 'templates')):
                 shutil.copytree(os.path.join(root, 'templates', i),
-                                os.path.join("my_project_1", 'templates', i))
+                                os.path.join("my_project", 'templates', i))
 
 except FileExistsError:
     print("Already created templates")
